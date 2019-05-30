54bdce9f1e784dcfacd3fca93f7b225920d444c3
jcmujica <jcmujica@gmail.com>
2019-05-30 11:15:48 -0400

    Updates

--- precache-manifest.f960e5f2a41081557688e1081fd2b3b3.js
+++ precache-manifest.f960e5f2a41081557688e1081fd2b3b3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbd0344a962a15d50d2ee9c3db363f34",
+    "revision": "1e5b9653b5cabfe795e04b4932f17bbd",
     "url": "/Proyecto-React-2/index.html"
   },
   {
-    "revision": "3e103e672d9b6f660670",
-    "url": "/Proyecto-React-2/static/css/main.9999d187.chunk.css"
+    "revision": "4ecb690b129feb78937d",
+    "url": "/Proyecto-React-2/static/css/main.14cd0e60.chunk.css"
   },
   {
     "revision": "8448bdf6c42a843327c3",
     "url": "/Proyecto-React-2/static/js/2.c831a00c.chunk.js"
   },
   {
-    "revision": "3e103e672d9b6f660670",
-    "url": "/Proyecto-React-2/static/js/main.e1e32008.chunk.js"
+    "revision": "4ecb690b129feb78937d",
+    "url": "/Proyecto-React-2/static/js/main.a17beff9.chunk.js"
   },
   {
     "revision": "8e2fb2eba2ede66c3f8e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Proyecto-React-2/precache-manifest.e515aff9be1dcf49987a000af51add56.js"
+  "/Proyecto-React-2/precache-manifest.f960e5f2a41081557688e1081fd2b3b3.js"
 );
 
 self.addEventListener('message', (event) => {
